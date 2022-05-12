75c71962d296711eab21fb5ceea761da0be16c4d
Jason <jason1382311@gmail.com>
2022-05-11 21:18:35 -0700

    fixed app bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React, {useState, useEffect} from "react";
-import {Container} from "@material-ui/core"
+import {AppBar, Button, Container, Toolbar, Typography} from "@material-ui/core"
+import useStyles from "./styles"
 function App() {
+    const classes = useStyles();
     return (
-        <Container >
-
+        <Container className = {classes.root}>
+            <AppBar className = {classes.appbar} elevation = {0}>
+                <Toolbar className = {classes.appbarWrapper}>
+                    <Typography variant = "h2" className = {classes.title}>Art</Typography>
+                    <Button variant = "outlined" className  = {classes.button}>
+                        Add Post
+                    </Button>
+                </Toolbar>
+                
+            </AppBar>
+            
         </Container>
     )
 }
