4d1bbcc3206f79208039d431ef2eb3b5d7cedd2c
Jason <jason1382311@gmail.com>
2022-05-11 23:21:43 -0700

    added button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,13 +2,13 @@
 import {AppBar, Button, Container, Grid, Toolbar, Typography} from "@material-ui/core"
 import useStyles from "./styles"
 
-function Header() {
+function Header(props) {
     const classes = useStyles();
     return (
         <AppBar className = {classes.appbar} elevation = {0}>
             <Toolbar className = {classes.appbarWrapper}>
                 <Typography variant = "h2" className = {classes.title}>Art</Typography>
-                <Button variant = "outlined" className  = {classes.button}>
+                <Button variant = "outlined" className  = {classes.button} onClick = {() => {props.setModal(true)}}>
                     Add Post
                 </Button>
             </Toolbar>
