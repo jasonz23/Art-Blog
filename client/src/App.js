1922f8fb298d44c9100e18181e854a26caa1c0a2
Jason <jason1382311@gmail.com>
2022-05-11 23:20:31 -0700

    added modal and fix layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
 import React, {useState, useEffect} from "react";
-import {AppBar, Button, Container, Grid, Toolbar, Typography, Grow} from "@material-ui/core"
+import {AppBar, Button, Container, Grid, Toolbar, Typography, Grow, Modal} from "@material-ui/core"
 import useStyles from "./styles"
 import Header from "./components/Header/Header"
+import PostModal from "./components/PostModal/PostModal"
+import {getPosts} from "./actions/posts";
+import { useDispatch } from "react-redux";
+import Posts from "./components/Posts/Posts"
 function App() {
     const classes = useStyles();
+    const [currentId, setCurrentId] = useState(null);
+    const [modal, setModal] = useState(false);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
     return (
         <Container className = {classes.root}>
-            <Header />
-            <Grow in>
-                <Container>
-                    <Grid>
+            <PostModal show = {modal} setModal = {setModal} currentId = {currentId} setCurrentId = {setCurrentId} />
+            <Header setModal = {setModal}/>
+            <Grid container className = {classes.grid}>
+                <Typography variant = "h2">
+                    Posts
+                </Typography>
+                <Grow in>
 
+                    <Grid container >
+                        <Posts setCurrentId = {setCurrentId} setModal = {setModal}/>
                     </Grid>
-                </Container>
+
             </Grow>
+            </Grid>
+            
+            
+            
         </Container>
+        
     )
 }
 
