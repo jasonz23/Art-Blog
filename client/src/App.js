892000fadec69e5d0255a383f870b05526ed9341
Jason <jason1382311@gmail.com>
2022-05-11 21:29:40 -0700

    removed header

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React, {useState, useEffect} from "react";
-import {AppBar, Button, Container, Toolbar, Typography} from "@material-ui/core"
+import {AppBar, Button, Container, Grid, Toolbar, Typography, Grow} from "@material-ui/core"
 import useStyles from "./styles"
+import Header from "./components/Header/Header"
 function App() {
     const classes = useStyles();
     return (
         <Container className = {classes.root}>
-            <AppBar className = {classes.appbar} elevation = {0}>
-                <Toolbar className = {classes.appbarWrapper}>
-                    <Typography variant = "h2" className = {classes.title}>Art</Typography>
-                    <Button variant = "outlined" className  = {classes.button}>
-                        Add Post
-                    </Button>
-                </Toolbar>
-                
-            </AppBar>
-            
+            <Header />
+            <Grow in>
+                <Container>
+                    <Grid>
+
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
