4fc2a00944d502bd35b0f296c48c2d0dcf425cba
Jason <jason1382311@gmail.com>
2022-05-11 20:07:37 -0700

    added basic code for app.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import {Container} from "@material-ui/core"
+function App() {
+    return (
+        <Container >
+
+        </Container>
+    )
+}
+
+export default App;
