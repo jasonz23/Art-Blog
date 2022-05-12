157844509734cbee33ec1320b017db88aa1f1291
Jason <jason1382311@gmail.com>
2022-05-11 21:19:02 -0700

    added styles

---
+++ client/src/styles.js
@@ -0,0 +1,33 @@
+import {makeStyles} from "@material-ui/core";
+
+export default makeStyles(() => ({
+    root: {
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    appbar: {
+        backgroundColor: "#234234"
+    },
+    appbarWrapper: {
+        width: "80%",
+        margin: "0 auto"
+    },
+    title: {
+        flex:"1"
+    },
+    button: {
+            "&.MuiButton-root": {
+                border: "2px white solid",
+                fontSize: "2vh",
+                fontFamily: "Signika",
+                color: "#fff",
+                width: "15vw",
+                border: "3px #ffff solid"
+
+            },
+            "&.MuiButton-outline": {
+                color: "#1c2321"
+            }
+        }
+}));
