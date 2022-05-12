a11ca3f37ba3cac3d8792a5d5948e85e478910b9
Jason <jason1382311@gmail.com>
2022-05-11 23:21:01 -0700

    added post layout

---
+++ client/src/actions/posts.js
@@ -0,0 +1,60 @@
+import * as api from "../api"
+//import everything from "../api"
+
+// Action Creators
+
+export const getPosts = () => async (dispatch) => {
+
+
+    try {
+        const {data} = await api.fetchPosts();
+        //fetch all the posts
+        dispatch({type: "FETCH_ALL", payload: data});
+        //data is the posts
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    
+    //Action creators are functions that return an action
+    //action has a type and a payload
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+        dispatch({type: "CREATE", payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const {data} = await api.updatePost(id,post);
+        //return updated memory
+        dispatch({type: "UPDATE", payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+        dispatch({type: "DELETE", payload: id});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const likePost = (id) => async(dispatch) => {
+    try {
+        const {data} = await api.likePost(id);
+        //return updated memory
+        dispatch({type: "LIKE", payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
