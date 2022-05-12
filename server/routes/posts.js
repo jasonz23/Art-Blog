a81fd878fcc5d0b1a169f22820b2f62ecc9d62be
Jason <jason1382311@gmail.com>
2022-05-11 23:23:19 -0700

    posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,1 +1,13 @@
 import express from "express";
+
+const router = express.Router();
+import {getPosts, createPost, updatePost, deletePost, likePost} from "../controllers/posts.js";
+
+router.get("/",getPosts);
+router.post("/",createPost);
+router.patch("/:id", updatePost);
+//patch is used to update existing documents
+router.delete("/:id",deletePost);
+router.patch("/:id/likePost", likePost);
+export default router;   
+ 
