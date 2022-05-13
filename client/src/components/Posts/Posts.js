accd864bf1f75886a832f3d6e45a617c4843d267
Jason <jason1382311@gmail.com>
2022-05-13 13:24:08 -0700

    imported mui styles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,8 +2,9 @@ import React from "react";
 import Post from "./Post/Post.js"
 import {useSelector} from "react-redux";
 import {Grid, CircularProgress} from "@material-ui/core";
-
+import useStyles from "./styles.js"
 function Posts(props) {
+    const classes  = useStyles();
     const posts = useSelector((state) => state.posts);
     console.log(posts);
     if (posts.length == 0) {
@@ -12,10 +13,10 @@ function Posts(props) {
         )
     } else {
         return (
-            <Grid container alignItems = "stretch" spacing = {3}>
+            <Grid container alignItems = "stretch" spacing = {3} className = {classes.root}>
 
                 {
-                    posts.map((post) => (
+                    posts.reverse().map((post) => (
                         <Grid key = {post.id} item xs = {12} sm  ={6}>
                             <Post post = {post} setCurrentId = {props.setCurrentId} setModal = {props.setModal}/>
                             {/* props drilling sending the props down the heirarchy */}
